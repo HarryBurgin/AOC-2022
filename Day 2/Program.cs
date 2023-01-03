ead561ba2e6a484efa4f80c6790ec8ecabf31687
HarryBurgin <79971535+HarryBurgin@users.noreply.github.com>
2023-01-03 20:02:16 +0000

    Added function to read the file

--- Day 2/Program.cs
+++ Day 2/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.IO;
 
 namespace Day_2
 {
     class Program
     {
+        public static string[] data;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ReadFile();
+        }
+
+        //Function that reads the text file containing the raw data
+        static string[] ReadFile()
+        {
+            string txtFile = @"Data.txt";
+            data = File.ReadAllLines(txtFile);
+
+            foreach (string line in data)
+                Console.WriteLine(line);
+            return data;
         }
     }
 }
