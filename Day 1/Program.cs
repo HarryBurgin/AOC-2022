e5abe3ad59846f505c8c83d9970647f10035bc21
HarryBurgin <79971535+HarryBurgin@users.noreply.github.com>
2023-01-03 19:31:17 +0000

    Added Calculate and Conversion methods

--- Day 1/Program.cs
+++ Day 1/Program.cs
@@ -5,20 +5,70 @@ namespace AOC_D1
 {
     class Program
     {
+        public static string[] data;
+        public static int[] calories;
         static void Main(string[] args)
         {
             Console.WriteLine("This is my solution for Day 1 of Advent Of Code 2022");
-            readFile();
+            ReadFile();
+            DataConversion();
+            Calculate();
         }
 
-        static void readFile()
+        //Function that reads the text file containing the raw data
+        static string[] ReadFile()
         {
             string txtFile = @"Data.txt";
-            string[] calories = File.ReadAllLines(txtFile);
+            data = File.ReadAllLines(txtFile);
 
-            foreach (string line in calories)
+            foreach (string line in data)
                 Console.WriteLine(line);
+            return data;
+        }
 
+        static int[] DataConversion()
+        {
+            //string d;
+            //int c;
+            //for (int i = 0; i < data.Length; i++)
+            //{
+            //    if (data[i] == "")
+            //        calories[i] = 0;
+            //    else
+            //    {
+            //        d = data[i];
+            //        c = Convert.ToInt32(d);
+            //        calories[i] = c;
+            //    }
+            //}
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == "")
+                    data[i] = "0";
+            }
+
+            calories = Array.ConvertAll(data, int.Parse);
+            return calories;
+        }
+
+        static void Calculate()
+        {
+            int current = 0;
+            int highestVal = 0;
+            int highestElf = 0;
+            for (int i = 0; i < calories.Length; i++)
+            {
+                if (calories[i] != 0)
+                    current += calories[i];
+                else if (current > highestVal)
+                {
+                    highestVal = current;
+                    highestElf = i + 1;
+                    current = 0;
+                }
+            }
+            Console.WriteLine("Elf " + highestElf + " had the most calories (" + highestVal + ")");
         }
     }
 }
