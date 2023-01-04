e4cdf610cc71a616d82d536a136ea6dc91b208e8
HarryBurgin <79971535+HarryBurgin@users.noreply.github.com>
2023-01-04 17:04:50 +0000

    Logic modified with split

--- Day 3/Program.cs
+++ Day 3/Program.cs
@@ -6,15 +6,14 @@ namespace Day_3
     class Program
     {
         public static string[] rucksacks;
-        public static string c1;
-        public static string c2;
         public static string[] lowers = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
             "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
         public static string[] uppers = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
             "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ReadFile();
+            Calculate();
         }
 
         static string[] ReadFile()
@@ -28,23 +27,26 @@ namespace Day_3
         static void Calculate()
         {
             int sum = 0;
+            string c1;
+            string c2;
             for (int i = 0; i < rucksacks.Length; i++)
             {
                 string sack = rucksacks[i];
                 c1 = sack.Substring(0, (sack.Length / 2) - 1);
-                c2 = sack.Substring(sack.Length / 2);
+                c2 = sack.Substring((sack.Length / 2) - 1);
 
                 for (int j = 0; j < c1.Length; j++)
                 {
                     if (c1[j] == c2[j])
                     {
                         if (Char.IsUpper(c1[j]))
-                            sum += (Array.IndexOf(uppers, c1[j]) + 1);
+                            sum += (Array.IndexOf(uppers, c1[j]) + 27);
                         else
                             sum += (Array.IndexOf(lowers, c1[j]) + 1);
                     }
                 }
             }
+            Console.WriteLine("The sum of the priorities is " + sum + "!");
         }
     }
 }
