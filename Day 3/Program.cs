a646e4209df7aea7c39112967ba9eaa4d480c604
HarryBurgin <79971535+HarryBurgin@users.noreply.github.com>
2023-01-04 16:35:15 +0000

    Added read file function

--- Day 3/Program.cs
+++ Day 3/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.IO;
 
 namespace Day_3
 {
     class Program
     {
+        public static string[] rucksacks;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        static string[] ReadFile()
+        {
+            string txtFile = @"data.txt";
+            rucksacks = File.ReadAllLines(txtFile);
+
+            return rucksacks;
         }
     }
 }
